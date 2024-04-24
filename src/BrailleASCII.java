46f39c7177564245cc981f013d6b333b0b139910
DAVID_RHOADES <RHOADESD@grinnell.edu>
2024-04-24 13:04:47 -0500

    Set up BrailleASCII

---
+++ src/BrailleASCII.java
@@ -0,0 +1,12 @@
+import java.io.PrintWriter;
+
+/**
+ * Allows for command line translation from braille bites to ASCII, ASCII to braille bits, or braille bits to Unicode braille symbols using BitTrees
+ * @author: David Rhoades 
+ */
+
+public class BrailleASCII {
+  public static void main(String[] args) throws Exception{
+    PrintWriter pen = new PrintWriter(System.out, true);
+  } // main
+}
--- src/BrailleASCIITables.java
+++ src/BrailleASCIITables.java
@@ -3,24 +3,12 @@ import java.io.File;
 import java.io.FileInputStream;
 
 /**
- * Allows for translation from braille bites to ASCII, ASCII to braille bits, or braille bits to Unicode braille symbols
+ * Allows for translation from braille bites to ASCII, ASCII to braille bits, or braille bits to Unicode braille symbols using BitTrees
  * @author David Rhoades
  */
 
 
 
 public class BrailleASCIITables {
-  public static void main(String[] args) throws Exception{
-    PrintWriter pen = new PrintWriter(System.out, true);
-
-    
-    BitTree tree = new BitTree(8);
-    File file = new File("src/ASCIItoBraille.txt");
-    tree.load(new FileInputStream(file));
-    tree.dump(pen);
-
-
-
-  } // main
 
 } // BrailleASCIITables
